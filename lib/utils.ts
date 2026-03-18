9804479619a2ba86333090cdd325cf5c7550d7b7
Davi Milioli <davi.milioli@inforce.com.br>
2026-03-18 12:32:43 -0300

    feat: utils para formatação de temperatura e vento

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatTemp = (temp: number | undefined): string | number => {
+  if(temp === undefined) return "--";
+  return Math.round(temp)
+}
+
+export const formatWind = (wind: number): number => {
+  return Math.round(wind * 3.6);
+}
