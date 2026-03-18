1054b053e39fc55253cf03641eca2b8e41985b47
Davi Milioli <davi.milioli@inforce.com.br>
2026-03-18 12:27:57 -0300

    feat: store de clima com zustand

---
+++ app/stores/weather.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+import { City } from "../types/City";
+
+type Store = {
+    data: City | null;
+    isLoading: boolean;
+    error: string | null;
+    setCity: (data: City) => void;
+    setLoading: (status: boolean) => void;
+    setError: (msg: string | null) => void;
+}
+
+export const useWeather = create<Store>((set) => ({
+    data: null,
+    isLoading: false,
+    error: null,
+    setCity: (data) => set({ data: data }),
+    setLoading: (status) => set({ isLoading: status }),
+    setError: (msg) => set({ error: msg })
+}));
