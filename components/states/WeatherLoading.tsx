93f3206fb98f5ab8dfdd9ed038a2783379332765
Davi Milioli <davimilioli2108@gmail.com>
2026-03-21 16:27:23 -0300

    refact: nome de componente LoadngSpiner para WeatherLoading e novo layout do componente

---
+++ components/states/WeatherLoading.tsx
@@ -0,0 +1,24 @@
+import { LoaderCircle } from "lucide-react";
+import { Spinner } from "../ui/spinner"
+
+
+type Props = {
+    message?: string
+    size: number;
+}
+
+const WeatherLoading = ({ size, message }: Props) => {
+    return (
+        <div className="flex flex-col items-center gap-2">
+            <div className="bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-800 rounded-xl-[28px] transition-all duration-300 rounded-xl p-2">
+                <Spinner className={`size-${size}`}>
+                    <LoaderCircle />
+                </Spinner>
+            </div>
+            {message && <p className="text-zinc-500 dark:text-zinc-400">{message}</p>}
+        </div>
+
+    )
+}
+
+export default WeatherLoading;
