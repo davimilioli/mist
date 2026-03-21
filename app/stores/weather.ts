bbf88557bd45b1e3fb69211127b10b105f0f2299
Davi Milioli <davimilioli2108@gmail.com>
2026-03-21 16:29:35 -0300

    refact: remoção data e setCity da store

--- app/stores/weather.ts
+++ app/stores/weather.ts
@@ -1,20 +1,15 @@
 import { create } from "zustand";
-import { WeatherData } from "@/types/Weather";
 
 type Store = {
-    data: WeatherData | null;
     isLoading: boolean;
     error: string | null;
-    setCity: (data: WeatherData) => void;
     setLoading: (status: boolean) => void;
     setError: (msg: string | null) => void;
 }
 
 export const useWeather = create<Store>((set) => ({
-    data: null,
     isLoading: false,
     error: null,
-    setCity: (data) => set({ data: data }),
     setLoading: (status) => set({ isLoading: status }),
     setError: (msg) => set({ error: msg })
 }));
